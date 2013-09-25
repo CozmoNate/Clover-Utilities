92e10b424edecc192b7e68001ec0f9541969a953
kozlek <natan.zalkin@me.com>
2013-09-26 00:35:08 +0300

    Refactoring, minor bugfixes. Integrated Sparkle autoupdater

---
+++ Sparkle/SilentUpdateDriver.h
@@ -0,0 +1,13 @@
+//
+//  SilentUpdateDriver.h
+//  Sparkle
+//
+//  Created by Kozlek on 26/09/13.
+//
+//
+
+#import "SUBasicUpdateDriver.h"
+
+@interface SilentUpdateDriver : SUBasicUpdateDriver
+
+@end
---
+++ Sparkle/SilentUpdateDriver.m
@@ -0,0 +1,13 @@
+//
+//  SilentUpdateDriver.m
+//  Sparkle
+//
+//  Created by Kozlek on 26/09/13.
+//
+//
+
+#import "SilentUpdateDriver.h"
+
+@implementation SilentUpdateDriver
+
+@end
